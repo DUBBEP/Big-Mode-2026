8828b7bbe81d520d1a12f8e7b0c5d86a6c6f2547
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2026-02-01 19:00:41 -0700

    Scene Configured

---
+++ Slick Floors/Assets/Scripts/Mopping/MopCleaningController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MopCleaningController : MonoBehaviour
+{
+    [SerializeField] private Collider2D mopTrigger;
+
+    private void Start()
+    {
+        if (mopTrigger == null) Debug.LogError($"No Mop Trigger set in Mop cleaning controller {name}");
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed)
+            mopTrigger.enabled = true;
+        else
+            mopTrigger.enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"Mop has entered trigger");
+        if (collision.TryGetComponent<FloorTile>(out FloorTile tile))
+        {
+            Debug.Log($"Mop has found tile");
+            tile.ChangeTyle(GroundType.Clean);
+        }
+    }
+}
