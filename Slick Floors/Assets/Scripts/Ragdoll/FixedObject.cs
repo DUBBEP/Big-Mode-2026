745e4edb58673f13af79c070b639e6e5b67c9393
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2026-02-01 17:33:37 -0700

    Restructured Files

--- Slick Floors/Assets/Scripts/Ragdoll/FixedObject.cs
+++ Slick Floors/Assets/Scripts/Ragdoll/FixedObject.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class FixedObject : MonoBehaviour
 {
     public Transform anchor;
-    public Transform fixedObject;
     public float strength;
     public float distanceFromAnchor = 0f;
 
     void Update()
     {
-        float distanceError = Vector2.Distance(fixedObject.position, anchor.position) - distanceFromAnchor;
-        Vector2 directionToAnchor = (anchor.position - fixedObject.position).normalized;
+        float distanceError = Vector2.Distance(transform.position, anchor.position) - distanceFromAnchor;
+        Vector2 directionToAnchor = (anchor.position - transform.position).normalized;
         Vector2 correctionForce = directionToAnchor * distanceError * strength;
         // handBody cannot be a rigidbody, so we move it directly
-        fixedObject.position += (Vector3)(correctionForce * Time.deltaTime);
+        transform.position += (Vector3)(correctionForce * Time.deltaTime);
     }
 }
