a7e001a7b208b5b7d003cb2e44e99f1bd8ef4b2a
BrentWeiffenbach <rweiffenbach@wpi.edu>
2026-02-07 13:03:55 -0500

    animation can't load hub

--- Slick Floors/Assets/Scripts/UI/ResultsScreenController.cs
+++ Slick Floors/Assets/Scripts/UI/ResultsScreenController.cs
@@ -42,6 +42,35 @@ public class ResultsScreenController : MonoBehaviour
     }
 
     // Call this method to progress through each result screen step
+    public void ProgressScreenAnimation()
+    {
+        if (minimumSkipTimer < minimumSkipDuration)
+            return;
+
+        switch (currentStep)
+        {
+            case 0:
+                SoundFXManager.Instance.playSoundFXClip(normalTextClip, transform, volume: 0.8f);
+                DisplayText(timeText);
+                break;
+            case 1:
+                SoundFXManager.Instance.playSoundFXClip(normalTextClip, transform, volume: 0.8f);
+                DisplayText(slicknessText);
+                break;
+            case 2:
+                SoundFXManager.Instance.playSoundFXClip(normalTextClip, transform, volume: 0.8f);
+                DisplayText(studentsText);
+                break;
+            case 3:
+                SoundFXManager.Instance.playSoundFXClip(thudClip, transform, volume: 0.8f);
+                DisplayText(gradeText);
+                break;
+        }
+
+        currentStep++;
+        minimumSkipTimer = 0f;
+    }
+
     public void ProgressScreen()
     {
         if (minimumSkipTimer < minimumSkipDuration)
@@ -66,6 +95,9 @@ public class ResultsScreenController : MonoBehaviour
                 DisplayText(gradeText);
                 break;
             case 4:
+                LoadHub();
+                return;
+            default:
                 LoadHub();
                 return;
         }
